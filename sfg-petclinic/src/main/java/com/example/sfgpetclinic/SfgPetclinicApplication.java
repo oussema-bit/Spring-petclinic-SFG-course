198cd0b2808ede88ce991a5c68f871de7e112ac8
oussema-bit <oussama.ayari1@esprit.tn>
2022-09-05 18:07:45 +0100

    Initial Project Commit

---
+++ sfg-petclinic/src/main/java/com/example/sfgpetclinic/SfgPetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.sfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgPetclinicApplication.class, args);
+	}
+
+}
